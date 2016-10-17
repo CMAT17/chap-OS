18256b0df51ce6be91b65e4490a3fa65ccf7285c
Sandwich <arm065339025@hotmail.com>
2016-10-17 17:07:51 -0500

    add comments to paging_h

--- chapOS/student-distrib/paging.h
+++ chapOS/student-distrib/paging.h
@@ -4,6 +4,7 @@
 #include "types.h"
 #include "x86_desc.h"
 
+//define all the magic numbers
 #define PAGE_DIRECTORY_SIZE 1024
 #define PAGE_TABLE_SIZE 1024
 #define PAGE_ALIGN 4096
@@ -12,6 +13,7 @@
 #define NOT_PRESENT 2
 #define INIT_4MB_KERNEL 0x400083
 
+//VIdo memory location to be map to page table (first page directory)
 #ifndef VIDEO
 #define VIDEO 0xB8000
 #endif
