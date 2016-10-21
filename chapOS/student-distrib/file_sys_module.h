a45e59012c10ae3320734ea9e11f421f1edd1807
Aaron Wang <aaronw94@gmail.com>
2016-10-20 22:18:47 -0500

    starting filesystems

---
+++ chapOS/student-distrib/file_sys_module.h
@@ -0,0 +1,10 @@
+#ifndef _FILE_SYS_H_
+#define _FILE_SYS_H_
+
+#include "types.h"
+
+int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry);
+int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry);
+int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
+
+#endif
